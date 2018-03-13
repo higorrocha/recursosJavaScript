6bae3e5bb2664fb113e06359e18cab95fb3bd1ad
higorrocha <higorrocha.14@gmail.com>
2018-03-13 09:17:47 -0300

    Operador de igualdade e desigualdade

---
+++ script.js
@@ -0,0 +1,10 @@
+// *Operador de igualdade e desigualdade*
+
+var primeira = '1' == 1;
+//compara a igualdade do valor da variável sem levar em consideração o seu tipo.
+var segunda = '2' === 2;
+//compara a igualdade do valor e o tipo da variável.
+var terceira = '3' != 3;
+//compara a desigualdade do valor da variável sem levar em consideração o seu tipo.
+var quarta = '4' !== 4;
+//compara a desigualdade do valor e o tipo da variável.
