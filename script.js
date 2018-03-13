2afeb0d1f9decceb4910895418e2936cf12b4dae
higorrocha <higorrocha.14@gmail.com>
2018-03-13 09:47:36 -0300

    O método substring()

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//O método substring()
+
+var exemplo = "JavaScript é show!";
+//criando a variável e alimentando com o texto desejado.
+var resultado = exemplo.substring(0, 10);
+/*O método substring recebe dois argumentos que determinarão 
+a posição inicial e final da substring que deseja selecionar - sendo que
+o segundo argumento é opcional.
+Nesse caso ela inicia a seleção na letra "J" e finaliza na letra "t",
+retornando a nova string "JavaScript". */
+
+/*
 // *Operador de igualdade e desigualdade*
 
 var primeira = '1' == 1;
@@ -8,3 +20,4 @@ var terceira = '3' != 3;
 //compara a desigualdade do valor da variável sem levar em consideração o seu tipo.
 var quarta = '4' !== 4;
 //compara a desigualdade do valor e o tipo da variável.
+*/
